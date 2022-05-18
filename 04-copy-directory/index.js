cba9afb6693a99cabb51d54524b410f957a7d8c0
pugocoder <57970164+pugocoder@users.noreply.github.com>
2022-05-18 18:49:00 +0300

    feat: add fourth task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 
+const dirPath = path.join(__dirname, 'files');
+const targetPath = path.join(__dirname, 'files-copy');
+
+fs.mkdir(targetPath, { recursive: true }, (err) => {
+  if (err) return console.error(err);
+
+  fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+    if (err) console.error(err);
+    else {
+      files.forEach((file) => {
+        fs.promises.copyFile(
+          path.join(dirPath, file.name),
+          path.join(targetPath, file.name)
+        );
+      });
+    }
+  });
+});
