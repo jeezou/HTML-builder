00abc5fa58f843e4f814a3987755892f49fdfac9
pugocoder <57970164+pugocoder@users.noreply.github.com>
2022-05-19 21:58:33 +0300

    feat: add clear before copy action in fourth task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,9 +4,7 @@ const path = require('path');
 const dirPath = path.join(__dirname, 'files');
 const targetPath = path.join(__dirname, 'files-copy');
 
-fs.mkdir(targetPath, { recursive: true }, (err) => {
-  if (err) return console.error(err);
-
+const copyFiles = () => {
   fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
     if (err) console.error(err);
     else {
@@ -19,5 +17,32 @@ fs.mkdir(targetPath, { recursive: true }, (err) => {
         }
       });
     }
+  });
+};
+
+fs.mkdir(targetPath, { recursive: true }, (err) => {
+  if (err) return console.error(err);
+
+  fs.readdir(targetPath, { withFileTypes: true }, (err, files) => {
+    if (err) return console.error(err);
+
+    const delPromises = [];
+
+    files.forEach((file) => {
+      delPromises.push(
+        new Promise((resolve, reject) => {
+          fs.unlink(path.join(targetPath, file.name), (err) => {
+            if (err) {
+              reject(err);
+              console.error(err);
+            } else {
+              resolve();
+            }
+          });
+        })
+      );
+    });
+
+    Promise.all(delPromises).then(copyFiles);
   });
 });
